9f99c2c353ba980d621d45c34fc31614dae5020c
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-09-12 22:03:53 +0900

    escape

---
+++ src/section02/Escape.java
@@ -0,0 +1,23 @@
+package section02;
+
+/*
+ * 이스케이프 (Escape) 문자
+ * 특정한 문자나 문자열을 특별한 용도로 해석하도록 하는 문자
+ * 
+ * \\ 역슬래시 자체
+ * \" 쌍따옴표
+ * \' 작은따옴표
+ * \n 줄바꿈
+ * \t 탭문자
+ * \r 캐리지 리턴
+ * \b 백스페이스
+ * 
+ */
+
+public class Escape {
+	public static void main(String[] args) {
+		System.out.println("Hello,World!");  //"Hello,World!"	
+		System.out.println("\" Hello,World!\"");
+	}
+
+}
